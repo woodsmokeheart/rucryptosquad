954adcdba982fc23c28ee50f529164011977fb4d
Denis Kukobin <denis.kukobin@hipasus.com>
2024-01-09 14:58:37 +0300

    deleted scroll cards in gallery

--- src/components/gallery/ResponsiveScreens/GalleryPageDesktop.tsx
+++ src/components/gallery/ResponsiveScreens/GalleryPageDesktop.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Header from "@/components/Header/Header";
-import ScrollCards from "../ScrollCards/ScrollCards";
 
 const GalleryPageDesktop: React.FC = () => {
   return (
     <div className="d-flex justify-center">
       <Header />
-      <ScrollCards />
     </div>
   );
 };
