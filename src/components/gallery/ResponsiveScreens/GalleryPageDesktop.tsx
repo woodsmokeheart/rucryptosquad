05c68cf6e0f2a68ffa3640130599c2f5966473a1
Denis Kukobin <denis.kukobin@hipasus.com>
2024-01-14 15:18:03 +0300

    added title for gallery tab

--- src/components/gallery/ResponsiveScreens/GalleryPageDesktop.tsx
+++ src/components/gallery/ResponsiveScreens/GalleryPageDesktop.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 const GalleryPageDesktop: React.FC = () => {
   return (
     <div className="d-flex justify-center">
+      <title>Gallery</title>
       <Header />
       <SliderGallery />
       <Image src={background} alt="hero" fill={true} loading="eager" />
