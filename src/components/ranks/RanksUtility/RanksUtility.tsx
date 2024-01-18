449ae94a7144314b7256865436862a79117ec806
Denis Kukobin <denis.kukobin@hipasus.com>
2024-01-18 16:14:49 +0300

    full page ranks

--- src/components/ranks/RanksUtility/RanksUtility.tsx
+++ src/components/ranks/RanksUtility/RanksUtility.tsx
@@ -4,8 +4,71 @@ import styles from "./RanksUtility.module.scss";
 
 export const RanksUtilityResident = () => {
   return (
-    <div>
-      <h2></h2>
+    <div className={styles.wrapper}>
+      <div className={styles.sub_title}>
+        "Resident" rank - receipt and utility
+      </div>
+      <p>
+        1. This token, issued by the project team, gives access to membership in
+        the DAO RCS
+      </p>
+      <p>
+        2. The token is awarded to a person who, in the opinion of the
+        Community, has been active in the cryptosphere or in other cases, such
+        as a gift raffle organized by the community or the wide fame and
+        authority of the invitee.
+      </p>
+      <p>
+        3. Upon receiving a token from the project team, the participant also
+        receives permission to use it fully for their own purposes. The
+        copyright of the image remains with the Community.
+      </p>
+      <p>
+        4. The Resident token can be purchased on the secondary market, but it
+        does not give the right to join the DAO, without the approval of the
+        community members of the person who bought the token, as well as after
+        they apply for membership. The utility remains unavailable and is not
+        provided until the person who bought the token is accepted into the
+        community.
+      </p>
+      <p>
+        5. If a token of this rank was sold by the owner on the secondary
+        market, was lost or donated, then re-obtaining an NFT of this rank is
+        possible only through a one-time contribution to the treasury of the
+        project of funds equivalent to 50% of the value of the token at the
+        moment.At the time of the "loss, sale, loss, etc." of the token, the
+        utility is terminated and the user is excluded from the closed chat
+        until a new, punitive purchase of a token of at least Resident rank. The
+        price tag is set directly by the treasurer, depending on the assessment
+        of the importance of the participant.
+      </p>
+      <div className={styles.sub_title}>Resident utility</div>
+      <p>
+        1. Access to a private chat that we are rightfully proud of and in which
+        participants throw out a lot of insiders.
+      </p>
+      <p>
+        2. Access to the RSS Hub (a unique NET exchanger that no one has
+        analogues of) in accordance with the rules of its use.
+      </p>
+      <p>
+        3. Access to sweepstakes of various valuable tokens aimed at RCS
+        members.
+      </p>
+      <p>
+        4. Access to membership auctions where various valuable tokens will be
+        presented.
+      </p>
+      <p>
+        5. The right to vote as a member of the DAO on all matters related to
+        the community.
+      </p>
+      <p>6. Getting OG and WL for all subsequent DAO collections.</p>
+      <p>
+        7. The transition to the Centurion rank is subject to the Conditions for
+        increasing the rank.
+      </p>
+      <h4>The list of utilities is not final, but it is minimal.</h4>
     </div>
   );
 };
@@ -13,7 +76,9 @@ export const RanksUtilityResident = () => {
 export const RanksUtilitySenator = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.sub_title}>This token can only be redeemed.</div>
+      <div className={styles.sub_title}>
+        "Senator" rank - receipt and utility
+      </div>
       <p>
         1. After purchasing this token from the project team, the user receives
         all the privileges that are tied to the rank of “Senator“, as well as
@@ -88,11 +153,11 @@ export const RanksUtilitySenator = () => {
         payments do not include Community collaborations or collections that are
         not focused on generating income for the community.
       </p>
-      <p>This list is not final, but it is minimal.</p>
-      <p>
+      <h4>This list is not final, but it is minimal.</h4>
+      <h4>
         By agreement and vote of the DAO, adjustments to the description are
         possible.
-      </p>
+      </h4>
     </div>
   );
 };
